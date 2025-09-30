162cde2f2c621ea4f34df7fa6ddb2e6c25a1cf2b
ficore-africa <ficoreafrica@gmail.com>
2025-09-30 16:36:20 +0100

    Update app.py

--- ficore_labs/app.py
+++ ficore_labs/app.py
@@ -25,7 +25,7 @@ from flask_limiter.util import get_remote_address
 from blueprints.users.routes import get_post_login_redirect
 from utils import (
     get_mongo_db, logger, initialize_tools_with_urls, generate_tools_with_urls,
-    TRADER_TOOLS, TRADER_NAV, ADMIN_TOOLS, ADMIN_NAV, format_date
+    TRADER_TOOLS, TRADER_NAV, ADMIN_TOOLS, ADMIN_NAV, TRADER_EXPLORE, ADMIN_EXPLORE, format_date
 )
 from translations import register_translation, trans, get_translations, get_all_translations, get_module_translations
 
